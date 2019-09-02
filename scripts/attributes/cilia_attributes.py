3071b175eecf097b2948c555324ff4f422e33f45
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-09-02 16:25:38 +0200

    Implement cilia features WIP

--- scripts/attributes/cilia_attributes.py
+++ scripts/attributes/cilia_attributes.py
@@ -2,6 +2,7 @@ from concurrent import futures
 import numpy as np
 import h5py
 import pandas as pd
+from elf.skeleton.teasar import Teasar
 
 
 def get_mapped_cell_ids(cilia_ids, manual_mapping_table_path):
@@ -16,32 +17,52 @@ def get_mapped_cell_ids(cilia_ids, manual_mapping_table_path):
     return cell_ids
 
 
-# TODO implement
-# more atttributes? curvature?
 def measure_cilia_attributes(seg_path, seg_key, base_table, resolution):
     n_features = 2
     attributes = np.zeros((len(base_table), n_features), dtype='float32')
     names = ['length', 'diameter']
 
-    # ids = base_table['cilia_ids'].values.astype('uiint64')
+    ids = base_table['label_id'].values.astype('uint64')
 
-    # with h5py.File(seg_path, 'r') as f:
-    #     ds = f[seg_key]
+    with h5py.File(seg_path, 'r') as f:
+        ds = f[seg_key]
 
-    #     def compute_attributes(cid):
-    #         # get the row for this cilia id
-    #         row = base_table[cid]
+        def compute_attributes(cid):
 
-    #         # compute the bounding box
+            # FIXME 1 and 2 should be part of bg label
+            if cid in (1, 2):
+                return
 
-    #         # load segmentation from the bounding box and get foreground
+            # get the row for this cilia id
+            row = base_table.loc[cid]
 
-    #         # compute len in microns (via shortest path) and diameter (via mean boundary distance transform)
+            # compute the bounding box
+            bb_min = (row.bb_min_z, row.bb_min_y, row.bb_min_x)
+            bb_max = (row.bb_max_z, row.bb_max_y, row.bb_max_x)
+            bb = tuple(slice(int(mi / re), int(ma / re))
+                       for mi, ma, re in zip(bb_min, bb_max, resolution))
 
-    #     n_threads = 8
-    #     with futures.ThreadPoolExecutor(n_threads) as tp:
-    #         tasks = [tp.submit(compute_attributes(cid)) for cid in ids[1:]]
-    #         [t.result() for t in tasks]
+            # load segmentation from the bounding box and get foreground
+            obj = ds[bb] == cid
+
+            # compute len in microns (via shortest path) and diameter (via mean boundary distance transform)
+            teasar = Teasar(obj, resolution)
+            src = teasar.root_node
+            target = np.argmax(teasar.penalized_distances)
+            path = teasar.get_path(src, target)
+            dist = teasar.get_pathlength(path)
+            print(path[:5])
+            print(dist)
+            # diameters = teasar.boundary_distances[path]
+            # diameter = np.mean(diameters)
+            attributes[cid, 0] = dist
+            # attributes[cid, 1] = diameter
+
+        [compute_attributes(cid) for cid in ids[1:]]
+        # n_threads = 8
+        # with futures.ThreadPoolExecutor(n_threads) as tp:
+        #     tasks = [tp.submit(compute_attributes(cid)) for cid in ids[1:]]
+        #     [t.result() for t in tasks]
 
     return attributes, names
 
---
+++ test/attributes/test_cilia.py
@@ -0,0 +1,21 @@
+import unittest
+import pandas as pd
+
+
+# check the basic / default attributes
+class TestCilaAttributes(unittest.TestCase):
+    def test_cilia_attributes(self):
+        from scripts.attributes.cilia_attributes import measure_cilia_attributes
+
+        input_path = '../../data/0.5.1/segmentations/sbem-6dpf-1-whole-segmented-cilia-labels.h5'
+        input_key = 't00000/s00/0/cells'
+        resolution = [0.025, 0.01, 0.01]
+        base_path = '../../data/0.5.1/tables/sbem-6dpf-1-whole-segmented-cilia-labels/default.csv'
+        base = pd.read_csv(base_path, sep='\t')
+
+        out = measure_cilia_attributes(input_path, input_key, base, resolution)
+        self.assertEqual(len(out), len(base))
+
+
+if __name__ == '__main__':
+    unittest.main()
