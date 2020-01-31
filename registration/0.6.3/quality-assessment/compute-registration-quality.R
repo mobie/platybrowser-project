66a08042d707ea2b6e988f9adac99f4c0b976e0e
Christian Tischer <christian.tischer@embl.de>
2020-01-31 15:19:45 +0100

    Update compute-registration-quality.R

--- registration/0.6.3/quality-assessment/compute-registration-quality.R
+++ registration/0.6.3/quality-assessment/compute-registration-quality.R
@@ -1,7 +1,15 @@
-# Command to produce the output points (example)
-# /Applications/elastix_macosx64_v4.8/run_transformix.sh -def /Users/tischer/Desktop/n.n.n/quality-assessment/em-fixed-points.txt -out /Users/tischer/Desktop/n.n.n/quality-assessment -tp /Users/tischer/Desktop/n.n.n/transformations/TransformParameters.BSpline10.9.9.9.txt
+# Commands to produce the output points on the terminal (examples)
+# 1. change dir into the folder with the transformation files; this is important because there are relative paths within the transformation files that rely on this.
+# cd  /Users/tischer/Desktop/0.6.3/transformations/ 
+# 2. run transformix to compute the transformed points
+# Rotation: /Applications/elastix_macosx64_v4.8/run_transformix.sh -def /Users/tischer/Desktop/0.6.3/quality-assessment/em-fixed-points.txt -out /Users/tischer/Desktop/0.6.3/quality-assessment -tp /Users/tischer/Desktop/0.6.3/transformations/TransformParameters.RotationPreAlign.0.0.0.txt
+# Similarity: /Applications/elastix_macosx64_v4.8/run_transformix.sh -def /Users/tischer/Desktop/0.6.3/quality-assessment/em-fixed-points.txt -out /Users/tischer/Desktop/0.6.3/quality-assessment -tp /Users/tischer/Desktop/0.6.3/transformations/TransformParameters.Similarity.9.9.9.txt
+# BSpline100: /Applications/elastix_macosx64_v4.8/run_transformix.sh -def /Users/tischer/Desktop/0.6.3/quality-assessment/em-fixed-points.txt -out /Users/tischer/Desktop/0.6.3/quality-assessment -tp /Users/tischer/Desktop/0.6.3/transformations/TransformParameters.BSpline100.9.9.9.txt
+# BSpline30: /Applications/elastix_macosx64_v4.8/run_transformix.sh -def /Users/tischer/Desktop/0.6.3/quality-assessment/em-fixed-points.txt -out /Users/tischer/Desktop/0.6.3/quality-assessment -tp /Users/tischer/Desktop/0.6.3/transformations/TransformParameters.BSpline30.9.9.9.txt
+# BSpline10: /Applications/elastix_macosx64_v4.8/run_transformix.sh -def /Users/tischer/Desktop/0.6.3/quality-assessment/em-fixed-points.txt -out /Users/tischer/Desktop/0.6.3/quality-assessment -tp /Users/tischer/Desktop/0.6.3/transformations/TransformParameters.BSpline10.9.9.9.txt
+input_folder = "/Users/tischer/Desktop/0.6.3/quality-assessment"
 
-file <- "/Users/tischer/Desktop/n.n.n/quality-assessment/outputpoints.txt"
+file <- paste(input_folder,"outputpoints.txt",sep="/")
 data <- read.table(file, sep = "" , header = F , nrows = 100,
                    na.strings ="", stringsAsFactors= F)
 
@@ -13,7 +21,7 @@ tz <- data[33] * 1000
 # manually assigned ProSPr
 library("readxl")
 
-file <- "/Users/tischer/Desktop/n.n.n/quality-assessment/manually_matched_points.xlsx"
+file <- paste(input_folder,"manually_matched_points.xlsx",sep="/") 
 data <- read_excel( file )
 
 mx = data[1] 
