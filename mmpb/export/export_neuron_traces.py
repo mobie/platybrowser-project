0ffb995dc0e59e78b6ea9375cb7bc95342868289
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2020-03-24 10:49:46 +0100

    Import all necessary files from branch david

--- mmpb/export/export_neuron_traces.py
+++ mmpb/export/export_neuron_traces.py
@@ -9,6 +9,7 @@ from skimage.draw import circle
 from pybdv.converter import make_scales
 from pybdv.metadata import write_xml_metadata, write_h5_metadata, write_n5_metadata, get_data_path
 from pybdv.util import get_key
+from tqdm import tqdm
 from ..util import is_h5_file
 
 
@@ -49,27 +50,43 @@ def extract_neuron_traces_from_nmx(trace_folder):
     return coords
 
 
-def write_vol_from_traces(traces, out_path, key, shape, resolution, chunks, radius):
+def write_vol_from_traces(traces, out_path, key, shape, resolution, chunks,
+                          radius, n_threads, crop_overhanging=True):
     # write temporary h5 dataset
     # and write coordinates (with some radius) to it
     with open_file(out_path) as f:
         ds = f.require_dataset(key, shape=shape, dtype='int16', compression='gzip',
                                chunks=chunks)
-        for nid, vals in traces.items():
+        ds.n_threads = n_threads
+        for nid, vals in tqdm(traces.items()):
             coords = vals_to_coords(vals, resolution)
             bb_min = coords.min(axis=0)
             bb_max = coords.max(axis=0) + 1
             assert all(bmi < bma for bmi, bma in zip(bb_min, bb_max))
-            assert all(b < sh for b, sh in zip(bb_max, shape))
+            this_trace = coords_to_vol(coords, nid, radius=radius)
 
-            sub_vol = coords_to_vol(coords, nid, radius=radius)
-            bb = tuple(slice(bmi, bma) for bmi, bma in zip(bb_min, bb_max))
-            ds[bb] += sub_vol
+            if any(b > sh for b, sh in zip(bb_max, shape)):
+                if crop_overhanging:
+                    crop = [max(int(b - sh), 0) for b, sh in zip(bb_max, shape)]
+                    print("Cropping by", crop)
+                    vol_bb = tuple(slice(0, sh - cr)
+                                   for sh, cr in zip(this_trace.shape, crop))
+                    this_trace = this_trace[vol_bb]
+                    bb_max = [b - crp for b, crp in zip(bb_max, crop)]
+                else:
+                    raise RuntimeError("Invalid bounding box: %s, %s" % (str(bb_max),
+                                                                         str(shape)))
+
+            bb = tuple(slice(int(bmi), int(bma)) for bmi, bma in zip(bb_min, bb_max))
+
+            sub_vol = ds[bb]
+            trace_mask = this_trace != 0
+            sub_vol[trace_mask] = this_trace[trace_mask]
+            ds[bb] = sub_vol
 
 
-# TODO this takes ages, somehow parallelize ! (need to lock somehow though)
 def traces_to_volume(traces, reference_vol_path, reference_scale, out_path,
-                     resolution, scale_factors, radius=5, chunks=None, n_threads=8):
+                     resolution, scale_factors, radius=2, chunks=None, n_threads=8):
     """ Export traces as segmentation compatible with the platy-browser.
     """
 
@@ -89,8 +106,11 @@ def traces_to_volume(traces, reference_vol_path, reference_scale, out_path,
 
     is_h5 = is_h5_file(out_path)
     key0 = get_key(is_h5, time_point=0, setup_id=0, scale=0)
-    write_vol_from_traces(traces, out_path, key0, shape, resolution, chunks, radius)
+    print("Writing traces ...")
+    write_vol_from_traces(traces, out_path, key0, shape, resolution, chunks, radius,
+                          n_threads)
 
+    print("Downscaling traces ...")
     make_scales(out_path, scale_factors, downscale_mode='max',
                 ndim=3, setup_id=0, is_h5=is_h5,
                 chunks=chunks, n_threads=n_threads)
@@ -100,15 +120,13 @@ def traces_to_volume(traces, reference_vol_path, reference_scale, out_path,
     bdv_res = [res / 1000. for res in resolution]
     unit = 'micrometer'
     write_xml_metadata(xml_path, out_path, unit, bdv_res, is_h5)
+    bdv_scale_factors = [[1, 1, 1]] + scale_factors
     if is_h5:
-        write_h5_metadata(out_path, scale_factors)
+        write_h5_metadata(out_path, bdv_scale_factors)
     else:
-        write_n5_metadata(out_path, scale_factors, bdv_res)
+        write_n5_metadata(out_path, bdv_scale_factors, bdv_res)
 
 
-# FIXME currently not working properly
-# TODO parallelize
-# TODO support passing a dict of seg-infos instead of hard-coding it to cell and nucleus seg
 def make_traces_table(traces, reference_scale, resolution, out_path, seg_infos={}):
     """ Make table from traces compatible with the platy browser.
     """
@@ -135,7 +153,8 @@ def make_traces_table(traces, reference_scale, resolution, out_path, seg_infos={
         datasets[seg_name] = ds
 
     table = []
-    for nid, vals in traces.items():
+    for nid, vals in tqdm(traces.items()):
+
         coords = vals_to_coords(vals, resolution)
         bb_min = coords.min(axis=0)
         bb_max = coords.max(axis=0) + 1
--- segmentation/traces/neuron_traces.py
+++ segmentation/traces/neuron_traces.py
@@ -1,8 +1,8 @@
 #! /g/arendt/EM_6dpf_segmentation/platy-browser-data/software/conda/miniconda3/envs/platybrowser/bin/python
 
 import json
+import os
 import numpy as np
-import pandas as pd
 from mmpb.export import extract_neuron_traces_from_nmx, traces_to_volume, make_traces_table
 
 
@@ -17,11 +17,25 @@ def get_resolution(scale, use_nm=True):
     return np.array(resolutions[scale])
 
 
+def get_traces(folder):
+    tmp_path = 'traces.json'
+    if os.path.exists(tmp_path):
+        with open(tmp_path) as f:
+            traces = json.load(f)
+        traces = {int(k): v for k, v in traces.items()}
+        return traces
+
+    traces = extract_neuron_traces_from_nmx(folder)
+    with open(tmp_path, 'w') as f:
+        json.dump(traces, f)
+    return traces
+
+
 def export_traces():
     folder = '/g/kreshuk/data/arendt/platyneris_v1/tracings/kevin'
     ref_path = '../../data/rawdata/sbem-6dpf-1-whole-raw.n5'
-    seg_out_path = './sbem-6dpf-1-whole-traces.n5'
-    table_out_path = './default.csv'
+    seg_out_path = '../../data/rawdata/sbem-6dpf-1-whole-traces.n5'
+    table_out_path = '../../data/rawdata/tables/sbem-6dpf-1-whole-traces/default.csv'
 
     ref_scale = 3
     cell_seg_info = {'path': '../../data/1.0.0/images/local/sbem-6dpf-1-whole-segmented-cells.xml',
@@ -30,51 +44,20 @@ def export_traces():
                         'scale': 0}
 
     print("Extracting traces ...")
-    traces = extract_neuron_traces_from_nmx(folder)
+    traces = get_traces(folder)
     print("Found", len(traces), "traces")
 
     resolution = get_resolution(ref_scale)
     n_scales = 4
     scale_factors = n_scales * [[2, 2, 2]]
     print("Write trace volume ...")
-    # traces_to_volume(traces, ref_path, ref_scale, seg_out_path, resolution, scale_factors)
+    traces_to_volume(traces, ref_path, ref_scale, seg_out_path, resolution, scale_factors)
 
     print("Make table for traces ...")
     make_traces_table(traces, ref_scale, resolution, table_out_path,
                       {'cell': cell_seg_info, 'nucleus': nucleus_seg_info})
 
 
-def get_cell_ids():
-    table_path = './sbem-6dpf-1-whole-traces-table-default.csv'
-    table = pd.read_csv(table_path, sep='\t')
-    cell_ids = table['cell_id'].values
-    cell_ids = cell_ids[cell_ids != 0].tolist()
-    with open('./trace_cell_ids.json', 'w') as f:
-        json.dump(cell_ids, f)
-
-
-# for debugging
-def check_extraction():
-    import elf.skeleton.io as skio
-    path = '/g/kreshuk/data/arendt/platyneris_v1/tracings/kevin/knm_ApNS_6dpf_neuron_traces.4138.nmx'
-    # path = '/g/kreshuk/data/arendt/platyneris_v1/tracings/comm_sec_seg.019.nmx'
-    skel = skio.read_nml(path)
-    search_str = 'neuron_id'
-    for k, v in skel.items():
-        sub = k.find(search_str)
-        beg = sub + len(search_str)
-        end = k.find('.', beg)
-        n_id = int(k[beg:end])
-
-        if n_id != 10:
-            continue
-
-        print(k)
-        print(n_id)
-        print(v)
-
 
 if __name__ == '__main__':
     export_traces()
-    # get_cell_ids()
-    # check_extraction()
