f492b77816289fc45695200fabd7f953dbc3357f
Constantin Pape <c.pape@gmx.net>
2020-05-27 00:27:35 +0200

    Update bookmarks

--- mobie/migrate_bookmarks.py
+++ mobie/migrate_bookmarks.py
@@ -1,14 +1,43 @@
 import json
 import os
+import subprocess
 from glob import glob
 
 from utils import to_lower
 
 
+def update_layers(layers):
+    new_layers = {}
+    for name, entries in layers.items():
+        updated_entries = {}
+        if 'minValue' in entries:
+            assert 'maxValue' in entries
+            clims = [float(entries.pop('minValue')), float(entries.pop('maxValue'))]
+            updated_entries['contrastLimits'] = clims
+        if 'colorMapMinValue' in entries:
+            assert 'colorMapMaxValue' in entries
+            clims = [float(entries.pop('colorMapMinValue')), float(entries.pop('colorMapMaxValue'))]
+            updated_entries['contrastLimits'] = clims
+        if 'colorMap' in entries:
+            updated_entries['color'] = entries.pop('colorMap')
+        updated_entries.update(entries)
+        new_layers[name] = updated_entries
+    return new_layers
+
+
 def update_bookmarks(bookmarks):
-    bookmarks = to_lower(bookmarks)
-    # TODO
     new_bookmarks = {}
+    for name, bkmrk in bookmarks.items():
+        bkmrk = to_lower(bkmrk)
+        if 'layers' in bkmrk:
+            layers = update_layers(bkmrk.pop('layers'))
+            new_bkmrk = {'layers': layers}
+        else:
+            new_bkmrk = {}
+        new_bkmrk.update(bkmrk)
+
+        new_bookmarks[name] = new_bkmrk
+
     return new_bookmarks
 
 
@@ -31,22 +60,24 @@ def migrate_bookmarks(misc_folder):
         json.dump(default_bookmark, f, indent=2, sort_keys=True)
 
     # copy the paper bookmarks
-    old_bookmark_path = os.path.join(bookmark_folder, 'bookmarks.json')
+    old_bookmark_path = os.path.join(misc_folder, 'bookmarks.json')
+    if not os.path.exists(old_bookmark_path):
+        return
+
     with open(old_bookmark_path) as f:
         bookmarks = json.load(f)
 
     bookmarks = update_bookmarks(bookmarks)
 
-    new_bookmark_path = os.path.join(misc_folder, 'manuscript_bookmarks.json')
+    new_bookmark_path = os.path.join(bookmark_folder, 'manuscript_bookmarks.json')
     with open(new_bookmark_path, 'w') as f:
         json.dump(bookmarks, f, indent=2, sort_keys=True)
 
-    # TODO
-    # subprocess.run(['git', 'rm', old_bookmark_path])
+    subprocess.run(['git', 'rm', old_bookmark_path])
 
 
 def migrate_all_bookmakrs(root):
-    folders = glob(os.path.join(root, "*.*"))
+    folders = glob(os.path.join(root, "*.*.*"))
     for folder in folders:
         misc_folder = os.path.join(folder, 'misc')
         migrate_bookmarks(misc_folder)
