1cb6a3fea5047c9771f3b3fea48ba0fc00619410
Christian Tischer <christian.tischer@embl.de>
2023-12-04 13:43:41 +0100

    adding python scripts

--- misc/add_image.py
+++ misc/add_image.py
@@ -1,40 +1,41 @@
 # Requirements:
 #
-# # mobie-utils-python
-# - git clone https://github.com/mobie/mobie-utils-python
-# - cd mobie-utils-python
-# - conda env create -f environment.yaml
-# - conda activate mobie
-# - cd mobie-utils-python
-# - pip install -e .
+# mobie-utils-python:
+# - conda create -c conda-forge mobie_utils
 #
-#
-# # platybrowser-project
+# platybrowser project:
 # - git clone https://github.com/mobie/platybrowser-project.git
-# - git checkout -b add_my_data
-#
+# - git checkout -b adding_data
 
 import mobie
 import mobie.metadata
+import h5py
+import tifffile
 
 image_name = 'em-raw-low-res-test'  # name in MoBIE; MUST be unique within the whole project!
 view = mobie.metadata.get_default_view("image", image_name, menu_name="sbem")
 
 print("adding image...")
 
+#image_data = tifffile.imread('/g/cba/exchange/em-raw-low-res-dxy1_28um-dz1_6um.tif')
+#with h5py.File("/g/cba/exchange/tmp.h5", "a") as f:
+#    f.create_dataset("data", data=image_data, compression="gzip")
+
+scale_factors = 2 * [[2, 2, 2]]
+
 mobie.add_image(
-    input_path='/Users/tischer/Documents/platybrowser-project/misc/em-raw-low-res-dxy1_28um-dz1_6um.tif',
-    input_key='',  # the input is a single tif image, so we leave input_key blank
+    input_path='/g/cba/exchange/tmp.h5',
+    input_key='data',  # the input is a single tif image, so we leave input_key blank
     file_format='ome.zarr',
-    root='/Users/tischer/Documents/platybrowser-project/data',
+    root='/g/cba/exchange/platybrowser-project/data',
     dataset_name='1.0.1',
     image_name=image_name,
     resolution=(1.6, 1.28, 1.28),
     chunks=(96, 96, 96),
-    scale_factors=1*[[2, 2, 2]],
+    scale_factors=scale_factors,
     unit='micrometer',
-    view=view
+    view=view,
+    target='slurm',
 )
 
 print("done!")
-
